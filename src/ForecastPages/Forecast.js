675f1f6c1db4af30244f3d720bd44fc9a82482ed
Max Twardowski <maxio1998@gmail.com>
2018-07-15 17:52:27 +0200

    Changed day detection so that it's more legit now

--- src/ForecastPages/Forecast.js
+++ src/ForecastPages/Forecast.js
@@ -3,21 +3,10 @@ import React from "react";
 const date = new Date();
 const today = date.getDay();
 
-var getDayName = dayID => {
-  switch (dayID) {
-    case 0: return 'Sunday';
-    case 1: return 'Monday';
-    case 2: return 'Tuesday';
-    case 3: return 'Wednesday';
-    case 4: return 'Thursday';
-    case 5: return 'Friday';
-    case 6: return 'Saturday';
-    default: break;
-  }
-}
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const Forecast = (props) => (
-  <p>{getDayName(today)}</p>
+  <p>{days[today]}</p>
 )
 
 export default Forecast;
